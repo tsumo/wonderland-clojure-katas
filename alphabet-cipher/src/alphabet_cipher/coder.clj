aa5e46590fb81d07a426322ed8e13db315a8a097
tsumo <tsumo@users.noreply.github.com>
2019-04-05 17:16:41 +0300

    alphabet-cipher solved

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,28 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet (cycle "abcdefghijklmnopqrstuvwxyz"))
+(def start (int \a))
+
+(defn- encode-one [k m]
+  (nth alphabet (+ (- (int k) start) (- (int m) start))))
+
 (defn encode [keyword message]
-  "encodeme")
+  (apply str (map encode-one (cycle keyword) message)))
+
+(defn- decode-one [k m]
+  (nth alphabet (+ 7 (int m) (- start (int k)))))
 
 (defn decode [keyword message]
-  "decodeme")
+  (apply str (map decode-one (cycle keyword) message)))
+
+(defn- find-cycle
+  ([code] (find-cycle code (count code) 1))
+  ([code length n]
+   (let [substring (subs code 0 n)]
+     (if (= code (apply str (take length (cycle substring))))
+       substring
+       (recur code length (inc n))))))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (find-cycle (apply str (map decode-one message cipher))))
 
